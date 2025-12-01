b23b66399f1c2bd90d96498db091ce49d62eea96
Dinotaurent <danieldazawow@gmail.com>
2025-12-01 18:46:41 -0500

    prueba exitosa, contenedor de backend, db y mongo express funcionando correctamente

--- src/config/db.py
+++ src/config/db.py
@@ -2,14 +2,17 @@ import os
 from pymongo import MongoClient
 
 # Leer variables de entorno
-# MONGO_USER = os.getenv("MONGO_USER")
-# MONGO_PASS = os.getenv("MONGO_PASS")
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
 MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
 MONGO_PORT = os.getenv("MONGO_PORT", "27017")
-MONGO_DB = os.getenv("MONGO_DB", "dbprueba")
+MONGO_DB = os.getenv("MONGO_DB", "notas-db")
 
 # Se crea la cadena de conexion
-MONGO_URI = f"mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}"
+# Antiguo: MONGO_URI = f"mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}"
+MONGO_URI = (f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}"
+             f"{MONGO_DB}?authSource=admin"
+             )
 
 # Crear conexion
 conn = MongoClient(MONGO_URI)
